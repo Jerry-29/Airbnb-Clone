47f823cfb58241ce94cd840ed9d2fb7e262c3784
Gopala <guptaraja727@gmail.com>
2022-02-24 09:56:29 +0530

    cc

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,16 @@
-export const Products = ()=>{
-    return <div>
-        <h1>Products</h1>
+import data from "../Lucknow.json";
+
+export const Products = () => {
+  console.log(data)
+  return (
+    <div>
+      {data.map((e)=>(
+       <h1>name:{e.name}-location:{e.location}</h1>
+      
+      ))}
+
     </div>
-}
+
+  );
+ 
+};
